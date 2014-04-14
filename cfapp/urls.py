756a41de758414164475cb37542bb37f45d47b3c
Mike Delaney <mdelaney@utexas.edu>
2014-04-14 16:28:58 -0500

    Add $ to admin url (test)

--- cfapp/urls.py
+++ cfapp/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns('',
     #url(r'^users/', include('users.urls', namespace="users")),
     url(r'^', include('users.urls', namespace="users")),
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^/admin/', include(admin.site.urls)),
+    url(r'^$/admin/', include(admin.site.urls)),
 )
